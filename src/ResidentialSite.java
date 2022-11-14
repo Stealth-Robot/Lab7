0f155b7767cb522bb5ed096ab909fc006a70756f
StealthRobot <kubiliusnick8@gmail.com>
2022-11-14 11:08:39 -0500

    extract to method refactor performed for getTaxAmount() in ResidentialSite

--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -1,11 +1,15 @@
 public class ResidentialSite extends Site {
     public double getBillableAmount(double _units, double _rate) {
         double base = getBaseAmount(_units, _rate);
-        double tax = base * Site.TAX_RATE;
+        double tax = getTaxAmount(_units, _rate);
         return base + tax;
     }
 
     private double getBaseAmount(double _units, double _rate) {
         return _units * _rate;
+    }
+
+    private double getTaxAmount(double _units, double _rate) {
+        return this.getBaseAmount(_units, _rate) * Site.TAX_RATE;
     }
 }
