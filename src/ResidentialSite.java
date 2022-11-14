9e60d9cdce8616c4c35e13cde461acb215689493
StealthRobot <kubiliusnick8@gmail.com>
2022-11-14 11:15:12 -0500

    extract to parent refactor performed for getBillableAmount() method to reduce repetition

--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -1,13 +1,9 @@
 public class ResidentialSite extends Site {
-    public double getBillableAmount(double _units, double _rate) {
-        return getBaseAmount(_units, _rate) + getTaxAmount(_units, _rate);
-    }
-
-    private double getBaseAmount(double _units, double _rate) {
+    protected double getBaseAmount(double _units, double _rate) {
         return _units * _rate;
     }
 
-    private double getTaxAmount(double _units, double _rate) {
+    protected double getTaxAmount(double _units, double _rate) {
         return this.getBaseAmount(_units, _rate) * Site.TAX_RATE;
     }
 }
--- src/Site.java
+++ src/Site.java
@@ -1,5 +1,12 @@
-public class Site {
+abstract class Site {
     protected static final double TAX_RATE = 0.13;
+
+    public double getBillableAmount(double _units, double _rate) {
+        return getBaseAmount(_units, _rate) + getTaxAmount(_units, _rate);
+    }
+
+    protected abstract double getTaxAmount(double units, double rate);
+    protected abstract double getBaseAmount(double units, double rate);
 
     public static void main(String[] args) {
         double _rate = 1527.25;
