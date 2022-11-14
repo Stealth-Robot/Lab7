7278aefba4a7b5d2485784b908a5327c3a173938
StealthRobot <kubiliusnick8@gmail.com>
2022-11-14 11:01:09 -0500

    extract to method refactor performed for getBaseAmount() in LifelineSite

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -1,7 +1,11 @@
 public class LifelineSite extends Site {
     public double getBillableAmount(double _units, double _rate) {
-        double base = _units * _rate * 0.5;
+        double base = getBaseAmount(_units, _rate);
         double tax = base * Site.TAX_RATE * 0.2;
         return base + tax;
+    }
+
+    private double getBaseAmount(double _units, double _rate) {
+        return _units * _rate * 0.5;
     }
 }
