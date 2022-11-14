84c993ffad5dcaddd859b02ba3d05fb08d820325
StealthRobot <kubiliusnick8@gmail.com>
2022-11-14 11:02:50 -0500

    extract to method refactor performed for getTaxAmount() in LifelineSite

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -1,11 +1,15 @@
 public class LifelineSite extends Site {
     public double getBillableAmount(double _units, double _rate) {
         double base = getBaseAmount(_units, _rate);
-        double tax = base * Site.TAX_RATE * 0.2;
+        double tax = getTaxAmount(_units, _rate);
         return base + tax;
     }
 
     private double getBaseAmount(double _units, double _rate) {
         return _units * _rate * 0.5;
+    }
+
+    private double getTaxAmount(double _units, double _rate) {
+        return this.getBaseAmount(_units, _rate) * Site.TAX_RATE * 0.2;
     }
 }
